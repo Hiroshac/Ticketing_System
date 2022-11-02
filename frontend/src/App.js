f443100c3a8deb70b4e31c7676f9ee7fd0ad34f2
Hiroshac <hiroshachandramal01@gmail.com>
2022-11-02 21:36:21 +0530

    ticketing app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,12 @@ import { Home } from "./component/admin/Home";
 import { AddInvalidToken } from "./component/admin/Inspector/AddInvalidToken";
 import { ViewInvalidToken } from "./component/admin/Inspector/ViewInvalidToken";
 import { AddTimeTables } from "./component/admin/PublicTransportManager/AddTimeTables";
+import Login from "./component/common/login/Login";
+// import { Login } from "./component/common/login/Login";
 import { NavBar } from "./component/common/NavBar";
+import { Registation } from "./component/common/registation/Registation";
+import { Visitorlog } from "./component/user/Guest/Visitorlog";
+import { Dashlocal } from "./component/user/Local/Dashlocal";
 
 function App() {
   return (
@@ -14,6 +19,11 @@ function App() {
         <Routes>
           {/* Client */}
           <Route path={'/'} exact element={<Home/>}/>
+          <Route path={'/reg'} exact element={<Registation/>}/>
+          <Route path={'/login'} exact element={<Login/>}/>
+          <Route path={'/localdash'} exact element={<Dashlocal/>}/>
+          <Route path={'/visitlog'} exact element={<Visitorlog/>}/>
+
           {/* <Route path={'/'} exact element={<Home/>}/> */}
           {/* Inspector */}
           <Route path={'/addinvalid'} exact element={<AddInvalidToken/>}/>
