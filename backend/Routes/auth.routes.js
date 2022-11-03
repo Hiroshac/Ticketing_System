0f277ac25442589ef5dfc6e322aa7741c3744e0a
Hiroshac <hiroshachandramal01@gmail.com>
2022-11-03 06:53:00 +0530

    changes

--- backend/Routes/auth.routes.js
+++ backend/Routes/auth.routes.js
@@ -3,8 +3,9 @@ import { authAPI } from '../Controllers/auth.controller.js';
 
 const router = express.Router();
 
+//registation
 router.post('/register', authAPI().add);
+//login
 router.post('/login', authAPI().log);
-
 
 export default router
